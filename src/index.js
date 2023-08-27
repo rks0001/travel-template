1d78d5e5c5cb8f972184308b608a0908ca2867c7
Ranjit Kalita <ranjitkalita499@gmail.com>
2023-08-27 19:15:21 +0530

    new-frontend-changes-26Aug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 

70ac928b1df6f5d5560c3bd411012b3fd543bf32
Ranjit Kalita <ranjitkalita499@gmail.com>
2023-08-27 19:20:01 +0530

    new-frontend-changes-26Aug

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { createRoot } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import ReactDOM from 'react-dom';  
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+const rootElement = document.getElementById('root');
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  rootElement
 );
